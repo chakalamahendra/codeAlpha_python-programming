37cb9be7fb7e0bc299baa5b94b10f12b393110f1
chakalamahendra <159121800+chakalamahendra@users.noreply.github.com>
2024-04-27 00:31:42 +0530

    Add files via upload

---
+++ virtual assis.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
+                print(command)
+    except sr.UnknownValueError:
+        pass
+    return command
+
+def run_jarvis():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'date' in command:
+        date = datetime.datetime.now().strftime('%d/%m/%Y')
+        talk('Today\'s date is ' + date)
+    elif 'how are you' in command:
+        talk('I am fine, thank you. How about you?')
+    elif 'what is your name' in command:
+        talk('I am Jarvis. What can I do for you?')
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    else:
+        talk('Please say that again.')
+
+run_jarvis()
